c63980090d8cf9c174fe494a23c65e9fff5e2d08
lochdown <lochdown@program>
2021-08-02 22:53:35 +0300

    i use new commit hash of deps-repl

--- program.sh
+++ program.sh
@@ -5,7 +5,13 @@ rename(){
 }
 
 repl(){
-  clj -M:repl
+  clj \
+    -X:repl deps-repl.core/process \
+    :main-ns deathstar.main \
+    :port 7788 \
+    :host '"0.0.0.0"' \
+    :repl? true \
+    :nrepl? false
 }
 
 main(){

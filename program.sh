5c9112f6e506d020a88fa94886ef88ae2b92fdd2
lochdown <lochdown@program>
2021-08-02 22:57:29 +0300

    in program.sh i leave only repl

--- program.sh
+++ program.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-rename(){
-    echo $1
-}
-
 repl(){
   clj \
     -X:repl deps-repl.core/process \
@@ -12,48 +8,6 @@ repl(){
     :host '"0.0.0.0"' \
     :repl? true \
     :nrepl? false
-}
-
-main(){
-  clojure \
-    -J-Dclojure.core.async.pool-size=1 \
-    -J-Dclojure.compiler.direct-linking=false \
-    -M -m deathstar.main
-}
-
-uberjar(){
-  mv target/lochdown.standalone.jar target/jpackage-input/
-}
-
-j-package(){
-  OS=${1:?"Need OS type (windows/linux/mac)"}
-
-  echo "Starting build..."
-
-  if [ "$OS" == "windows" ]; then
-    J_ARG="--win-menu --win-dir-chooser --win-shortcut"
-          
-  elif [ "$OS" == "linux" ]; then
-      J_ARG="--linux-shortcut"
-  else
-      J_ARG=""
-  fi
-
-  APP_VERSION=0.1.0
-
-  jpackage \
-    --input target/jpackage-input \
-    --dest target \
-    --main-jar lochdown.standalone.jar \
-    --name "lochdown" \
-    --main-class clojure.main \
-    --arguments -m \
-    --arguments lochdown.main \
-    --resource-dir resources \
-    --java-options -Xmx2048m \
-    --app-version ${APP_VERSION} \
-    $J_ARG
-  
 }
 
 "$@"

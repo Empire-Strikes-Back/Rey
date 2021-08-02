e4ffb8b91e8d06357081f41b4e104de43a392fc2
lochdown <lochdown@program>
2021-08-02 13:01:36 +0300

    i remove cljfx

--- src/lochdown/main.clj
+++ src/lochdown/main.clj
@@ -7,50 +7,21 @@
                                      pipeline pipeline-async]]
    [clojure.string]
    [clojure.spec.alpha :as s]
-   [clojure.java.io :as io]
-   [cljfx.api])
+   [clojure.java.io :as io])
   (:import
-   (javafx.event Event EventHandler)
-   (javafx.stage WindowEvent)
-   (javafx.scene.control DialogEvent Dialog ButtonType ButtonBar$ButtonData)
-   #_javafx.application.Platform
    (com.sun.jna Library Native Platform #_Function NativeLibrary)))
 
 (println "clojure.compiler.direct-linking" (System/getProperty "clojure.compiler.direct-linking"))
 (clojure.spec.alpha/check-asserts true)
 (do (set! *warn-on-reflection* true) (set! *unchecked-math* true))
 
-(s/def ::searchS string?)
-
-(defn stage
-  [{:as opts
-    :keys [::searchS]}]
-  {:fx/type :stage
-   :showing true
-   #_:on-close-request #_(fn [^WindowEvent event]
-                           (println :on-close-request)
-                           #_(.consume event))
-   :width 1024
-   :height 768
-   :scene {:fx/type :scene
-           :root {:fx/type :h-box
-                  :children [{:fx/type :label :text "rename"}
-                             {:fx/type :text-field
-                              :text searchS}]}}})
 (defonce stateA (atom nil))
 
 (defn -main [& args]
   (println ::-main)
-  (let [data-dir (-> (io/file (System/getProperty "user.dir")) (.getCanonicalPath))
-        renderer (cljfx.api/create-renderer)]
-    (reset! stateA {:fx/type stage
-                    ::searchS ""
-                    ::renderer renderer})
-    (add-watch stateA :watch-fn (fn [k stateA old-state new-state] (renderer new-state)))
-
-    (javafx.application.Platform/setImplicitExit true)
-    (renderer @stateA)
-    #_(cljfx.api/mount-renderer stateA render)
+  (let [data-dir (-> (io/file (System/getProperty "user.dir")) (.getCanonicalPath))]
+    (reset! stateA {})
+    (add-watch stateA :watch-fn (fn [k stateA old-state new-state] new-state))
 
     (go)))
 
@@ -77,11 +48,7 @@
 
   (-main)
 
-  (def renderer (::renderer @stateA))
-
-  (renderer @stateA)
-
-  (swap! stateA assoc ::searchS "123")
+  (swap! stateA assoc ::random (rand-int 1000))
 
   (.isInterface lochdown.main.ICLibrary)
 
